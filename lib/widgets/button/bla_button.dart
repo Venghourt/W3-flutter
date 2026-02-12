a49233e55ef50c943960596963596a39a2ce8d64
Chhoeun Venghourt <hourt2133@gmail.com>
2026-02-12 14:15:42 +0700

    BLA-002 – Implement BlaButton

--- lib/widgets/button/bla_button.dart
+++ lib/widgets/button/bla_button.dart
@@ -7,34 +7,50 @@ class BlaButton extends StatelessWidget {
   final IconData? icon;
   final String label;
   final ButtonVariant variant;
+  final VoidCallback? onPressed;
 
   const BlaButton({
     super.key,
     required this.label,
     required this.variant,
+    this.onPressed,
     this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
     final bool isPrimary = variant == ButtonVariant.primary;
-
     final backgroundColor = isPrimary
         ? BlaColors.backGroundColor
         : BlaColors.backgroundAccent;
-
     final contentColor = isPrimary ? BlaColors.white : BlaColors.primary;
 
-    return ElevatedButton.icon(
-      style: ElevatedButton.styleFrom(backgroundColor: backgroundColor),
-      onPressed: () {},
-      icon: icon != null
-          ? Icon(icon, color: contentColor)
-          : const SizedBox.shrink(),
-      label: Text(
-        label,
-        style: BlaTextStyles.label.copyWith(color: contentColor),
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(BlaSpacings.radius),
+        ),
+        minimumSize: const Size.fromHeight(48),
       ),
+      child: icon != null
+          ? Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon, color: contentColor),
+                const SizedBox(width: 8),
+                Text(
+                  label,
+                  style: BlaTextStyles.label.copyWith(color: contentColor),
+                ),
+              ],
+            )
+          : Text(
+              label,
+              style: BlaTextStyles.label.copyWith(color: contentColor),
+            ),
     );
   }
 }
